cb44308aba5f2fd4fcd471787cf5dbc3e51d9883
DhruviPatel <245122748042@mvsrec.edu.in>
2025-07-23 10:15:41 +0530

    Add files via upload

---
+++ StudentDetails.java
@@ -0,0 +1,10 @@
+public class StudentDetails{
+	public static void main(String[] args){
+		String name="Dhruvi";
+		String rollno="245122748042";
+		String department="cse(ai/ml)";
+		System.out.println("student name:"+name);
+		System.out.println("Roll Number:"+rollno);
+		System.out.println("Department:"+department);
+	}
+}
